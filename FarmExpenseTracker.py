776c7c2e07337fa02963dff6c6c4d6f48bdda5aa
SamCory1 <samcory908@gmail.com>
2024-11-02 13:29:02 -0500

    Changes by arena

--- FarmExpenseTracker.py
+++ FarmExpenseTracker.py
@@ -1,10 +1,12 @@
+from dataclasses import dataclass
+
 run = True
 crops = ["corn", "wheat", "sweet corn", "cotton", "apple", "rice", "soybean", "potato", "melon", "hay", "sweet potato"]
 budget = 1000
-corn =
+corn = 113.5
 print("The current budget is: $", budget)
 input("What crop is this?").lower()
 while run:
     answer = input("What crop would you like to buy?").lower()
-        if answer in crops and not "end":
+    if answer in crops and not "end":
             acre = input("How many acres would you like to plant on?")
